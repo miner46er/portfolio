19fb3bceb28a6e18adda618f536628e6b6ff15e9
Stefanus Ardi Mulia <stefanus.ardimulia@gmail.com>
2020-06-09 16:34:12 +0700

    feat: add new project and project photos

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -19,16 +19,23 @@ function Projects () {
           source=''
         />
         <Project
+          name='Diario'
+          description='A task reminder app with designed to be fun to use and with modern user interface. Built using Unity in C#.'
+          image='/projects/diario.jpg'
+          demo=''
+          source=''
+        />
+        <Project
           name='Cindua Mato'
-          description='2D action-adventure game about folklore from Minangkabau created using Unity in C#.'
+          description='2D action-adventure game about folklore from Minangkabau. Built using Unity in C#.'
           image='/projects/cindua_mato.jpg'
           demo=''
           source=''
         />
         <Project
           name='Me, You & Our Dream'
-          description='A platformer game project created using Unity in C#. '
-          image='/projects/our_dream.jpg'
+          description='A platformer game project. Built using Unity in C#. '
+          image='/projects/me_you_&_our_dream.jpg'
           demo='https://sorrowinrain.itch.io/me-you-our-dream'
           source=''
         />
